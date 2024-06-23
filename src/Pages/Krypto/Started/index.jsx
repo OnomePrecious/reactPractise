2ef6ca0ea2ab635d05b6adae3dcd81a20d901674
Precious <preciousonome002@gmail.com>
2024-06-23 18:24:57 +0100

    practise

--- src/Pages/Krypto/Started/index.jsx
+++ src/Pages/Krypto/Started/index.jsx
@@ -6,11 +6,16 @@ const Started =() => {
             <div className={style.mainContainer}>
                 <div className={style.textContainer}>
                     <h4> ARE YOU READY?</h4>
-                    <p style={{color: "#fff", display: "flex", justifyContent: "center"}}>Be A Part Of The <br/> Next Big Thing</p>
+                    <p style={{color: "#fff", display: "flex", justifyContent: "center"}}>Be A Part Of The <br/> Next
+                        Big Thing</p>
                 </div>
-                    <button className={style.buttonSection}>
-                        GET STARTED
-                    </button>
+                {/*<button className={style.buttonSection}>*/}
+                {/*    GET STARTED*/}
+                {/*</button>*/}
+
+                <button style={{position: "absolute",  left: "50%", padding: "10px 20px", color: "#fff", display: "flex", justifyContent: "center"}}>
+                    GET STARTED </button>
+
 
             </div>
         </>
