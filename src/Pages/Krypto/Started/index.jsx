2d0a0c34685a336819406304fb8deb8508cbfc44
Precious <preciousonome002@gmail.com>
2024-06-23 18:33:48 +0100

    practise

--- src/Pages/Krypto/Started/index.jsx
+++ src/Pages/Krypto/Started/index.jsx
@@ -9,12 +9,10 @@ const Started =() => {
                     <p style={{color: "#fff", display: "flex", justifyContent: "center"}}>Be A Part Of The <br/> Next
                         Big Thing</p>
                 </div>
-                {/*<button className={style.buttonSection}>*/}
-                {/*    GET STARTED*/}
-                {/*</button>*/}
+                <button className={style.buttonSection}>
+                    GET STARTED
+                </button>
 
-                <button style={{position: "absolute",  left: "50%", padding: "10px 20px", color: "#fff", display: "flex", justifyContent: "center"}}>
-                    GET STARTED </button>
 
 
             </div>
